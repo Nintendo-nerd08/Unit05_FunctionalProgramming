d29379f9e14149364283d4ee748743c141664c3d
lloydkev000 <lloydkevin631@gmail.com>
2025-02-24 13:54:40 -0500

    done

--- src/GCD.java
+++ src/GCD.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class GCD {
+    public static void main(String[] args){
+
+        GCDCalculator gcd = (a, b) -> {
+            while (b != 0){
+                int oldB = b;
+                b = a % b;
+                a = oldB;
+            }
+            return a;
+        };
+    }
+    @FunctionalInterface
+    public interface GCDCalculator{
+        int gcd(int a, int b);
+
+    }
 }
+
+// gdc of 12, 8, 8 is what?
+// 12 -> 2, 2, 3
+//8 -> 2, 2,
+// gdc 2 * 2 = 4
